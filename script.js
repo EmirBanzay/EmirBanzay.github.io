ccd9a6a8f882a86f79267c9fe8c86726ea58dd1b
EmirBanzay <emirmakh@gmail.com>
2025-07-02 12:07:37 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const open=document.querySelector(".lulululuaaalul")
+console.log(document)
+const dududu=document.querySelector(".mane")
+open.addEventListener("click",function(){
+    dududu.classList.toggle("active")
+})
